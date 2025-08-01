3c7af7e8e0ad90951d10fe7be0a258d970b4f6a6
Doran342545345 <105158686+Doran342545345@users.noreply.github.com>
2025-08-02 00:32:07 +0200

    Create RemoveStun.lua

---
+++ RemoveStun.lua
@@ -0,0 +1,50 @@
+local player = game.Players.LocalPlayer
+local targetFolder = workspace:WaitForChild("Live"):WaitForChild(player.Name)
+
+local blacklist = {
+    ["ForwardDashCD"] = true,
+    ["DashCD"] = true,
+    ["DashPunchCD"] = true,
+    ["RecentStun"] = true,
+    ["RecentStunNoAction"] = true,
+    ["DASHCD"] = true,
+    ["RecentDash"] = true,
+    ["Dashing"] = true,
+    ["BDStun"] = true,
+    ["DontAllowBlocking"] = true,
+    ["CantPunchONCLIENT"] = true,
+    ["Stun"] = true,
+    ["TRUECANTSIDEDASH"] = true,
+    ["recentdashok"] = true,
+    ["SideDashCounter"] = true,
+    ["CantBeHitByPunches"] = true,
+    ["IFrame2"] = true,
+    ["Ok"] = true,
+    ["InterruptEffectsVal"] = true,
+    ["BlueFlamesDashPunch"] = true,
+    ["TargetFoundBD"] = true,
+    ["ResettingTimer"] = true,
+	["AirDASHCD"] = true,
+	["NoDashingTrue"] = true,
+	["EventInteractableCDTree"] = true,
+	["IFrame"] = true,
+
+	}
+
+-- Remove existing bad instances just in case
+for _, child in pairs(targetFolder:GetChildren()) do
+    if blacklist[child.Name] then
+        child:Destroy()
+        print("Removed existing: " .. child.Name)
+    end
+end
+
+-- Automatically remove any blacklisted instance as soon as it's added
+targetFolder.ChildAdded:Connect(function(child)
+	task.wait()
+    if blacklist[child.Name] then
+        child:Destroy()
+        print("Removed instantly: " .. child.Name)
+    end
+end)
+getgenv().RemoveStun = true
